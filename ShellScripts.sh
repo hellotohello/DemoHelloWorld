942a7029935832a107e1a5fc5dd0d5e2cad6b04b
hellotohello <45407783+hellotohello@users.noreply.github.com>
2020-05-18 15:07:21 +0530

    Update ShellScripts.sh

--- ShellScripts.sh
+++ ShellScripts.sh
@@ -33,7 +33,7 @@ startTomcat()
  echo "tomcat is already running"
  else
  echo "tomcat is not running going to start"
- #chmod 777 -R /opt/tomcat
+
  sh /opt/tomcat/bin/startup.sh
  echo "tomcat start up"
  sleep 20
